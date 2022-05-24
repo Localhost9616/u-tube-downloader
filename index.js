4182dae4a62e8615dcea8b7ccddc15becbd8dc17
Localhost9616 <localhost9616@gmail.com>
2022-05-25 00:27:20 +0530

    Youtube Downloader Website
    This website helps in downloading Youtube videos in less time and efforts.

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const download=(link, format) =>{
+    console.log("running2");
+    document.querySelector('button').innerHTML=('<iframe style="height: 50px; border:none; overflow:hidden;" src="https://loader.to/api/button/?url='+ link + '&f=' + format + '"></iframe>');
+}
+document.querySelector('button').addEventListener("click", ()=>{
+    console.log("running");
+    const link = document.querySelector('.link').value;
+    const format = document.querySelector('.format').value;
+    download(link,format);
+})
+// const but=document.getElementById("myBtn");
+// but.addEventListener("click", displayDate);
+
+function displayDate() {
+  document.getElementById("myBtn").innerHTML = Date();
+}
